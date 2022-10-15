6aa6a6decf1ff3daeb6208522af79905ee51427d
mixnuts07 <french0719@gmail.com>
2022-10-15 13:48:53 +0900

    [add]setup GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,9 +3,22 @@ import "../styles/tailwind.css";
 // TODO react styleで条件で変更。ここは消してindex.tsxで実装
 import "../styles/style.css";
 import "../i18n/config";
+import { useEffect } from "react";
+import { useRouter } from "next/router";
+import * as gtag from "../lib/gtag";
 
 // Next.jsで全ページ共通の処理を挟むために書く
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url);
+    };
+    router.events.on("routeChangeComplete", handleRouteChange);
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
   return <Component {...pageProps} />;
 }
 
