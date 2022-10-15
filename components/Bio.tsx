b95fb291734ed9a2ee7a0f552e5a235532bf1098
mixnuts07 <french0719@gmail.com>
2022-10-15 14:11:26 +0900

    test]

--- components/Bio.tsx
+++ components/Bio.tsx
@@ -4,7 +4,7 @@ const Bio = ({ t }) => {
       <h3 className="title-section">Bio</h3>
       <div className="animate-fadeInSection">
         <h5>2021.03</h5>
-        <h3>で{t("bio.1")}</h3>
+        <h3>{t("bio.1")}</h3>
         <h5>2021.04</h5>
         <h3>{t("bio.2")}</h3>
         <h5>2022.07</h5>
